d2df90ccd2a8fd6ed17ea734c4af05be79eaf8b7
James Glover <jg16@sanger.ac.uk>
2012-02-02 10:17:02 +0000

    Improved robustness of xml fetching system. Should handle errors better.

--- pt_api.rb
+++ pt_api.rb
@@ -6,40 +6,97 @@ module PtApi
   
 
   def self.fetch_story(id)
-    resource_uri = URI.parse("http://www.pivotaltracker.com/services/v3/projects/#{$PROJECT_ID}/stories/#{id}")
-    proxy = URI.parse(ENV['HTTP_PROXY'])
-    proxy_class = Net::HTTP::Proxy(proxy.host,proxy.port)
+    
     if (!test?) || (test? && id!='2')
-      response = proxy_class.start(resource_uri.host, resource_uri.port) do |http|
-        http.get(resource_uri.path, {'X-TrackerToken' => $API_TOKEN})
-      end
-      puts response.body
-      data = REXML::Document.new(response.body)
+      resource_uri = URI.parse("http://www.pivotaltracker.com/services/v3/projects/#{$PROJECT_ID}/stories/#{id}")
+      proxy = URI.parse(ENV['HTTP_PROXY'])
+      proxy_class = Net::HTTP::Proxy(proxy.host,proxy.port)
+      data = fetch_xml(resource_uri,proxy_class)
     else
-        file = File.open("./test/xml/returned.xml")
-        data = REXML::Document.new(file.read())
-        file.close()
-    end
-
-    db_story=data.elements.each("story") do |rec_story|
-      ticket_id = rec_story.elements["id"].text
-      date = DateTime.parse(rec_story.elements["created_at"].text)
-      
-      begin
-        db_story = Story.create!(
-        :ticket_id => ticket_id,
-        :name => rec_story.elements["name"].text,
-        :created => date,
-        :ticket_type => rec_story.elements["story_type"].text
-        )
-      rescue # Just in case
-        db_story = Story.find_by_ticket_id(id)
-        db_story.created = date
-        db_story.save
-      end
-      return db_story
+        begin
+          file = File.open("./test/xml/returned.xml")
+          data = REXML::Document.new(file.read())
+        ensure
+          file.close()
+        end
     end
     
+    story_hash = { # Create a hash with out default data
+      :ticket_id => id,
+      :name => "Unknown story",
+      :ticket_type => "unknown",
+      :created => DateTime.parse("01/01/2000")
+    }
+    
+    if data==nil
+      # Do nothing
+    else
+      if check_xml(data,id)
+        parse_xml(data,story_hash)
+      else
+        puts "XML is invalid or does not match story"
+      end
+    end
+    
+    db_story = create_story(story_hash,id)
+    return db_story # Explicity for future reference 
+  end
+  
+  def self.fetch_xml(uri,proxy)
+    @retried = false
+    begin
+      response = proxy.start(uri.host, uri.port) do |http|
+        http.get(uri.path, {'X-TrackerToken' => $API_TOKEN})
+      end
+      return REXML::Document.new(response.body)
+    rescue
+          if !@retried
+            puts "Could not connect to API: retrying"
+            @retried = true
+            retry
+          else
+            puts "Could not connect to API: failed"
+            return nil
+          end
+    end
+  end
+  
+  def self.check_xml(data,id)
+    if data.root == nil || data.root.name != 'story' || data.root.elements["id"] == nil || data.root.elements["id"].text != id
+      return false # The XML is invalid
+    else
+      return true
+    end
+  end
+  
+  def self.parse_xml(data,hash)
+    begin
+      hash[:name] = data.root.elements["name"].text
+      hash[:created] = DateTime.parse(data.root.elements["created_at"].text)
+      hash[:ticket_type] = data.root.elements["story_type"].text
+    rescue
+      puts "XML passed validation but could not be parsed: Check API changes"
+    end
+  end
+  
+  def self.create_story(hash,id)
+    begin
+      db_story = Story.create!(
+        :ticket_id => hash[:ticket_id],
+        :name => hash[:name],
+        :created => hash[:created],
+        :ticket_type => hash[:ticket_type]
+      )
+    rescue # Just in case
+      db_story = Story.find_by_ticket_id(id)
+      if db_story != nil #Ie. The story has been create in the meantime
+       db_story.created = hash[:created] if db_story.created == nil
+       db_story.name = hash[:name] if db_story.name == nil
+       db_story.ticket_type = hash[:ticket_type] if db_story.ticket_type == nil
+       db_story.save
+     end
+    end
     return db_story
   end
+    
 end
